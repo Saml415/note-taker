2e481cdf36a36cf38827e7f4056eb566973193aa
Sam Lloyd <bobdoe784@gmail.com>
2021-07-15 18:15:06 -0700

    Adding routes to db.json

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const app = express();
 app.use(express.static("public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
-
+app.get('/api/notes', (req,res) => res.sendFile(path.join(__dirname, './db/db.json')))
+app.post('/api/notes', (req,res) => console.log(req.body))
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
